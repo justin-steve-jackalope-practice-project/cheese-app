0a48b770cb467e921d89045a5cddd8ff0f891dff
Steve Pflug <stephen@pflug.us>
2023-11-27 16:05:39 -0600

    completed tests and functions for cheesesToStr and filterCheesesByType

--- utils.js
+++ utils.js
@@ -1,6 +1,18 @@
 // NAME_OF_CHEESE_1 from COUNTRY_OF_ORIGIN_1 (TYPE_1)\n
 
 
-export function obToStrCheese(ob) {
-    return `${ob.name} from ${ob.country_of_origin} (${ob.type})`;
+export function obToStrCheese(cheese) {
+    return `${cheese.name} from ${cheese.country_of_origin} (${cheese.type})`;
+}
+
+export function cheesesToStr(cheeses) {
+    return cheeses.map(obToStrCheese).join('\n');
+}
+
+Array.prototype.filterCheesesByType = filterCheesesByType;
+
+export function filterCheesesByType(type) {
+    return this.filter(cheese => {
+        return cheese.type === type;
+    });
 }
--- utils.test.js
+++ utils.test.js
@@ -1,5 +1,5 @@
 import {describe, expect, test} from "vitest";
-import {obToStrCheese} from "./utils.js";
+import {obToStrCheese, cheesesToStr} from "./utils.js";
 
 const cheese = {
     "name": "Cashel Blue",
@@ -7,9 +7,42 @@ const cheese = {
     "country_of_origin": "Ireland"
 };
 
+const cheeses = [
+    cheese,
+    {
+        "name": "Taleggio",
+        "type": "semi-soft",
+        "country_of_origin": "Italy"
+    }
+];
+
+const oneCheese = [
+    cheese
+];
+
+const threeCheeses = [
+    ...cheeses,
+    {
+        "name": "Manouri",
+        "type": "soft",
+        "country_of_origin": "Greece"
+    }
+];
+
+const fourCheeses = [
+    ...threeCheeses,
+    {
+        "name": "Queso Fresco",
+        "type": "soft",
+        "country_of_origin": "Mexico"
+    }
+];
+
+
+// Tests
 
 describe('obToStrCheese', () => {
-    test('it should work', () => {
+    test('it should be a defined function', () => {
         expect(obToStrCheese).toBeDefined();
     });
     test('it should return a string', () => {
@@ -26,5 +59,50 @@ describe('obToStrCheese', () => {
     });
     test('it should be the correct format', () => {
         expect(obToStrCheese(cheese)).toBe('Cashel Blue from Ireland (blue)');
+    });
+});
+
+describe('cheesesToStr', () => {
+    test('it should be a defined function', () => {
+        expect(cheesesToStr).toBeDefined();
+    });
+    test('it should concatenate two cheeses when passed an array of two cheeses', () => {
+        expect(cheesesToStr(cheeses)).toBe('Cashel Blue from Ireland (blue)\nTaleggio from Italy (semi-soft)');
+    });
+    test('it should return one cheese string formatted correctly', () => {
+        expect(cheesesToStr(oneCheese)).toBe('Cashel Blue from Ireland (blue)');
+    });
+    test('it should concatenate three cheeses when passed an array of three cheeses', () => {
+        expect(cheesesToStr(threeCheeses)).toBe('Cashel Blue from Ireland (blue)\nTaleggio from Italy (semi-soft)\nManouri from Greece (soft)');
+    });
+    test('it should still work when passed a zero length array of cheeses', () => {
+        expect(cheesesToStr([])).toBe('');
+    });
+});
+
+describe('filterCheesesByType', () => {
+    test('it should be defined', () => {
+        expect(threeCheeses.filterCheesesByType).toBeDefined();
+    });
+    test('it should return Manouri cheese object when filtering by "soft" type from three cheese array', () => {
+        expect(threeCheeses.filterCheesesByType('soft')).toStrictEqual([{
+            "name": "Manouri",
+            "type": "soft",
+            "country_of_origin": "Greece"
+        }]);
+    });
+    test('it should return Manouri and Queso Fresco cheese objects when filtering by "soft" type from four cheese array', () => {
+        expect(fourCheeses.filterCheesesByType('soft')).toStrictEqual([
+            {
+                "name": "Manouri",
+                "type": "soft",
+                "country_of_origin": "Greece"
+            },
+            {
+                "name": "Queso Fresco",
+                "type": "soft",
+                "country_of_origin": "Mexico"
+            }
+        ]);
     });
 });
